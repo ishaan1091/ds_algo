c7388e4bfe2cc4b99428773f98d85c455c824bd2
Ishaan Kamra <ishaan1091@gmail.com>
2021-05-19 22:46:09 +0530

    minor changes

--- arrays/maximumProductSubarray.cpp
+++ arrays/maximumProductSubarray.cpp
@@ -1,42 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Discuss with jatin as it is wrong
-// long long int maxProduct(int arr[], int n)
-// {
-//     long long int prod = 1, maxProd = arr[0];
-//     for (int i = 0; i < n; i++)
-//     {
-//         prod *= arr[i];
-//         cout << arr[i] << " : " << prod << endl;
-//         if (prod > maxProd)
-//         {
-//             maxProd = prod;
-//         }
-//         if (prod == 0)
-//         {
-//             prod = 1;
-//         }
-//     }
-//     cout << "maxProd" << maxProd;
-//     prod = 1;
-//     for (int j = n - 1; j >= 0; j--)
-//     {
-//         prod *= arr[j];
-//         cout << arr[j] << " : " << prod << endl;
-//         if (prod > maxProd)
-//         {
-//             maxProd = prod;
-//         }
-//         if (prod == 0)
-//         {
-//             prod = 1;
-//         }
-//     }
-//     cout << "maxProd" << maxProd;
-//     return maxProd;
-// }
-
 long long maxProduct(int *arr, int n)
 {
     long long minVal = arr[0];
